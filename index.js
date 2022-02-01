1af81605fb1fd752b72450334f1c84d62803a4e3
Martin <nitromagix@gmail.com>
2022-01-31 19:12:46 -0800

    wrote for and while loops

--- index.js
+++ index.js
@@ -1,5 +1,62 @@
+
 let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-// Your Code Here
+/* for (let i = 0; i < weeklyExpenseQuestions.length; i++){
+    let weqAnswer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + weqAnswer
+}
+
+for (let i = 0; i < monthlyExpenseQuestions.length; i++){
+    let meqAnswer = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
+    monthlyExpenses = monthlyExpenses + meqAnswer
+}
+
+for (let i = 0; i < annualExpenseQuestions.length; i++){
+    let aeqAnswer = parseFloat(window.prompt(annualExpenseQuestions[i]))
+    annualExpenses = annualExpenses + aeqAnswer 
+}*/
+/* console.log(weqAnswer) */
+
+
+// ********* REVERSE LOOP  **********
+
+/* for (let i = weeklyExpenseQuestions.length - 1; i >=0; i--){
+    let weqAnswer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + weqAnswer
+}
+
+for (let i = weeklyExpenseQuestions.length - 1; i >=0; i--){
+    let meqAnswer = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
+    monthlyExpenses = monthlyExpenses + meqAnswer
+}
+
+for (let i = weeklyExpenseQuestions.length - 1; i >=0; i--){
+    let aeqAnswer = parseFloat(window.prompt(annualExpenseQuestions[i]))
+    annualExpenses = annualExpenses + aeqAnswer
+} */
+
+
+// ********* WHILE LOOP  **********
+
+let weeklyIndex = 0
+while (weeklyIndex < weeklyExpenseQuestions.length){
+    let weqAnswer = parseFloat(window.prompt(weeklyExpenseQuestions[weeklyIndex]))
+    weeklyExpenses = weeklyExpenses + weqAnswer
+    weeklyIndex++
+}
+
+let monthlyIndex = 0
+while (monthlyIndex < monthlyExpenseQuestions.length){
+    let meqAnswer = parseFloat(window.prompt(monthlyExpenseQuestions[monthlyIndex]))
+    monthlyExpenses = monthlyExpenses + meqAnswer
+    monthlyIndex++
+}
+
+let annualIndex = 0
+while (annualIndex < annualExpenseQuestions.length){
+    let aeqAnswer = parseFloat(window.prompt(annualExpenseQuestions[annualIndex]))
+    annualExpenses = annualExpenses + aeqAnswer
+    annualIndex++
+}
